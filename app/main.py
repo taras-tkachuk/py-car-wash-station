369d494410ef9d68645c82f42965975e3257b3e3
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-11 21:26:15 +0300

    fix code style

--- app/main.py
+++ app/main.py
@@ -16,16 +16,23 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> int:
-        return round(sum(
-            [self.wash_single_car(car)
-             for car in cars if car.clean_mark < self.clean_power]
-        ), 1)
+        return round(
+            sum(
+                [self.wash_single_car(car)
+                 for car in cars
+                 if car.clean_mark < self.clean_power]
+            ),
+            1
+        )
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class
-                     * (self.clean_power - car.clean_mark)
-                     * self.average_rating
-                     / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center,
+            1
+        )
 
     def wash_single_car(self, car: Car) -> float:
         washing_price = self.calculate_washing_price(car)
@@ -33,7 +40,9 @@ class CarWashStation:
         return washing_price
 
     def rate_service(self, score: int) -> None:
-        self.average_rating = round((self.average_rating
-                                    * self.count_of_ratings + score)
-                                    / (self.count_of_ratings + 1), 1)
+        self.average_rating = round(
+            (self.average_rating * self.count_of_ratings + score)
+            / (self.count_of_ratings + 1),
+            1
+        )
         self.count_of_ratings += 1
